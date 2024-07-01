afefc9a20d73df17a943e1dba60279fded1fd586
puja.morvadiya <puja.moravadiya2024@gmail.com>
2024-07-01 14:05:08 +0530

    added

--- src/Components/TicTacToe.tsx
+++ src/Components/TicTacToe.tsx
@@ -16,6 +16,9 @@ const TicTacToe: React.FC = () => {
     const [xIsNext, setXIsNext] = useState<boolean>(true);
     const [squares, setSquares] = useState<string[]>(Array(9).fill(null));
     function handleClick(i: number) {
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         const nextSquares = squares.slice();
         if (xIsNext) {
             nextSquares[i] = "X";
@@ -24,6 +27,28 @@ const TicTacToe: React.FC = () => {
         }
         setSquares(nextSquares);
         setXIsNext(!xIsNext);
+        const winner = calculateWinner(squares);
+        console.log(winner);
+
+    }
+    function calculateWinner(squares: string[]) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
     }
     return (
         <div className="container">
