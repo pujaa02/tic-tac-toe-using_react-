32b86996b0f4a09082657ba6531c736b9c4d1ba1
puja.morvadiya <puja.moravadiya2024@gmail.com>
2024-07-01 12:39:36 +0530

    added

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Board from "./Components/tictactoe";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/TicTacToe.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./tictactoe.css";
+
+function Square({ value, onSquareClick }) {
+    return (
+        <button className="square" onClick={onSquareClick}>
+            {value}
+        </button>
+    );
+}
+
+const TicTacToe: React.FC = () => {
+    const [xIsNext, setXIsNext] = useState<boolean>(true);
+    const [squares, setSquares] = useState<string[]>(Array(9).fill(null));
+    function handleClick(i: number) {
+        const nextSquares = squares.slice();
+        if (xIsNext) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
+        setSquares(nextSquares);
+        setXIsNext(!xIsNext);
+    }
+    return (
+        <div>
+            <div className="board-row">
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+            </div>
+            <div className="board-row">
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+            </div>
+            <div className="board-row">
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+            </div>
+        </div>
+    );
+}
+
+export default TicTacToe;
